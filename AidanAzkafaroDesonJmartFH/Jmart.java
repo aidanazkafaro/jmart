12aba8c12d9f9828bf037cd31ed1bf47aa43ba17
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2021-09-16 13:21:06 +0700

    modul 1 - revisi 2

--- AidanAzkafaroDesonJmartFH/Jmart.java
+++ AidanAzkafaroDesonJmartFH/Jmart.java
@@ -2,34 +2,85 @@ package AidanAzkafaroDesonJmartFH;
 
 
 /**
- * Write a description of class Jmart here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Aidan Azkafaro Deson/2006528862)
+ * 
  */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
+    public static int getPromo()
     {
-        // initialise instance variables
-        x = 0;
+        return 0;
     }
+    
+    public static String getCustomer()
+    {
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage(int before, int after)
+    {
+        float beforeFloat = before;
+        float afterFloat = after;
+        
+        if(before < after)
+        {
+            return 0.0f;
+        }
+        return  ((beforeFloat-afterFloat)/beforeFloat)*100.0f;
+    }
+    
+    public static int getDiscountedPrice(int price, float discountPercentage)
+    {
+        
+        float priceFloat = price;
+        
+        if(discountPercentage > 100.0f)
+        {
+            return 0;
+        }
+        float hasil = priceFloat - (priceFloat * (discountPercentage/100));
+        int hasilInt = (int)hasil;
+        return hasilInt;
+    
+    } 
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage)
+    {
+        float discountedPriceFloat = discountedPrice;
+        float hasil = (100.0f/(100.0f - discountPercentage)) * discountedPrice;
+        return (int)hasil;
+    }
+    
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public static float getCommissionMultiplier()
     {
-        // put your code here
-        return x + y;
+        return 0.05f;
     }
+    
+    public static int getAdjustedPrice(int price)
+    {
+        float priceFloat = price;
+        float hasil = priceFloat + (priceFloat * getCommissionMultiplier());
+        
+        return (int)hasil;
+    }
+    
+    public static int getAdminFee(int price)
+    {
+        float priceFloat = price;
+        float hasil = priceFloat * getCommissionMultiplier();
+        return (int)hasil;
+    }
+    
+    public static void main (String[]args){
+        System.out.println(getDiscountPercentage(1000,900));
+        System.out.println(getDiscountedPrice(1000,120.0f));
+        System.out.println(getOriginalPrice(1000,0));
+        System.out.println(getCommissionMultiplier());
+        System.out.println(getAdjustedPrice(0));
+        System.out.println(getAdminFee(500));
+    }
+    
 }
