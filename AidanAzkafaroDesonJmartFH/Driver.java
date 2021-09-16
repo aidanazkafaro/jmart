5711ccd0a69ce2c80415c3791428d79415343d18
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2021-09-16 09:36:41 +0700

    modul 1 - revisi

--- AidanAzkafaroDesonJmartFH/Driver.java
+++ AidanAzkafaroDesonJmartFH/Driver.java
@@ -9,30 +9,7 @@ package AidanAzkafaroDesonJmartFH;
  */
 public class Driver
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Driver
-     */
-    public Driver()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-    
     public static int getPromo()
     {
         return 0;
@@ -45,47 +22,66 @@ public class Driver
     
     public static float getDiscountPercentage(int before, int after)
     {
+        float beforeFloat = before;
+        float afterFloat = after;
+        
         if(before < after)
         {
             return 0.0f;
         }
-        return  ((before-after)/before)*100;
+        return  ((beforeFloat-afterFloat)/beforeFloat)*100.0f;
     }
     
     public static int getDiscountedPrice(int price, float discountPercentage)
     {
         
         float priceFloat = price;
+        
         if(discountPercentage > 100.0f)
         {
             return 0;
         }
-        int hasil = price - (price* discountPercentage);
-        return hasil;
+        float hasil = priceFloat - (priceFloat * (discountPercentage/100));
+        int hasilInt = (int)hasil;
+        return hasilInt;
     
     } 
     
     public static int getOriginalPrice(int discountedPrice, float discountPercentage)
     {
-        return discountPrice + (100+discountPercentage)*;
+        float discountedPriceFloat = discountedPrice;
+        float hasil = (100.0f/(100.0f - discountPercentage)) * discountedPrice;
+        return (int)hasil;
     }
     
+
     public static float getCommissionMultiplier()
     {
         return 0.05f;
     }
     
+    public static int getAdjustedPrice(int price)
+    {
+        float priceFloat = price;
+        float hasil = priceFloat + (priceFloat * getCommissionMultiplier());
+        
+        return (int)hasil;
+    }
     
     public static int getAdminFee(int price)
     {
-        return price * int(getCommissionMultiplier());
+        float priceFloat = price;
+        float hasil = priceFloat * getCommissionMultiplier();
+        return (int)hasil;
     }
     
     public static void main (String[]args){
         System.out.println(getDiscountPercentage(1000,900));
-        System.out.println("tes");
-        
-        System.out.println(getAdminFee(1000));
+        System.out.println(getDiscountedPrice(1000,120.0f));
+        System.out.println(getOriginalPrice(1000,0));
+        System.out.println(getCommissionMultiplier());
+        System.out.println(getAdjustedPrice(0));
+        System.out.println(getAdminFee(500));
     }
     
 }
