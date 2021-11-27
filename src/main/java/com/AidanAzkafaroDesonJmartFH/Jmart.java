3e2572d2056f8a6a1833133180d3bf7398f2d153
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2021-11-27 12:31:30 +0700

    Modul 9 - Aidan - CS (2)

--- src/main/java/com/AidanAzkafaroDesonJmartFH/Jmart.java
+++ src/main/java/com/AidanAzkafaroDesonJmartFH/Jmart.java
@@ -37,10 +37,9 @@ public class Jmart {
     }
 
 	public static void main(String[] args) {
-		JsonDBEngine.Run(Jmart.class);
+		
 		SpringApplication.run(Jmart.class, args);
-		Runtime.getRuntime().addShutdownHook(new Thread(()-> JsonDBEngine.join()));
-
+		
 	}
 
 }
--- src/main/java/com/AidanAzkafaroDesonJmartFH/Payment.java
+++ src/main/java/com/AidanAzkafaroDesonJmartFH/Payment.java
@@ -3,38 +3,30 @@ package com.AidanAzkafaroDesonJmartFH;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Write a description of class Payment here.
- *
- * @author (Aidan Azkafaro Deson)
- * @version (a version number or a date)
- */
-public class Payment extends Invoice
-{
-    public ArrayList<Record> history = new ArrayList<Record>();
-    public int productCount;
-    public Shipment shipment;
+public class Payment extends Invoice {
+	public ArrayList<Record> history;
+	public int productCount;
+	public Shipment shipment;
 
-    public Payment(int buyerId, int productId, int productCount, Shipment shipment){
-        super(buyerId, productId);
-        this.productCount = productCount;
-        this.productId = productId;
-        this.shipment = shipment;
-    }
+	public Payment(int buyerId, int productId, int productCount, Shipment shipment) {
+		super(buyerId, productId);
+		this.productCount = productCount;
+		this.shipment = shipment;
+	}
 
-    public double getTotalPay(Product product){
-        return product.price * product.discount;
-    }
+	public double getTotalPay(Product product) {
+		return product.price + product.shipmentPlans;
+	}
 
-    public static class Record{
-        public Status status;
-        public Date date;
-        public String message;
+	public static class Record {
+		public Date date;
+		public String message;
+		public Status status;
 
-        public Record(Status status, String message){
-            this.status = status;
-            this.message = message;
-            this.date = java.util.Calendar.getInstance().getTime();
-        }
-    }
+		public Record(Status status, String message) {
+			this.status = status;
+			this.message = message;
+			this.date = new Date();
+		}
+	}
 }
